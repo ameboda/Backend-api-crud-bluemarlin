6c8a9624f77450a50514e4b4b300de011491bbc0
SistemasBlueMarlin <sistemas1@bluemarlin.com.co>
2024-02-23 09:39:51 -0500

    encriptacion contraseña

--- src/app/domain/usecases/SellersCase/save-seller.usecase.ts
+++ src/app/domain/usecases/SellersCase/save-seller.usecase.ts
@@ -1,38 +1,52 @@
 import { inject, injectable } from "inversify";
 import SellerGateway from "../../models/sellers/gateway/sellers.gateway";
 import { sellersModel } from "../../../domain/models/sellers/sellers.model"
+import bcrypt from "bcrypt" ; 
+
 
 @injectable()
 export class SavesellerUsecase {
   constructor(
     @inject("SellerGateway") private sellerGateway: SellerGateway
-  ) {}
-   async invoke(param: sellersModel): Promise<sellersModel> {
+  ) { }
+
+  async invoke(param: sellersModel): Promise<sellersModel> {
     let responseBD: any;
-    let responseSellerUsecase:any; 
-    let responsegetSellercc= await this.sellerGateway.getBycc(param.cc); 
-    let responsegetSellereemail= await this.sellerGateway.getByemail(param.email); 
+    let responseSellerUsecaseemail: any;
+    let responseSellerUsecasecc: any;
+    let responsegetSellercc = await this.sellerGateway.getBycc(param.cc);
+    let responsegetSellereemail = await this.sellerGateway.getByemail(param.email);
 
-    //  validacion numero cedula
-    if(responsegetSellercc){
-      responseSellerUsecase= {
-        error: 'ya existe este numero de cedula'
-      }
-      return responseSellerUsecase; 
+    // encriptacion de contraseña con bycrypt 
+    
+    const saltrounds= 10; //cantidad de hash que deseo crear
+    const salt= await bcrypt.genSalt(saltrounds) //generacion del hash en la variable saltrounds
+    const hashedPassword= await bcrypt.hash(param.password, salt) //realiza el hashing en la contraseña ,param contraseña plana y salt la cadena aleatoria generada
+    param.password= hashedPassword
 
-      //validacion por correo 
-   
-      
-    }else if(responsegetSellereemail){
-      responseSellerUsecase={
-        error: 'ya existe este correo'
-      }
-      return responseSellerUsecase; 
-      //guardar usuario
-    }else{
-      responseBD= this.sellerGateway.save(param); 
-      return responseBD; 
+
+    // Validación email y cedula con switch:
+    switch (true) {
+
+      case !!responsegetSellereemail:
+        responseSellerUsecaseemail = {
+          error: 'ya existe este correo'
+        };
+        return responseSellerUsecaseemail;
+
+      case !!responsegetSellercc: 
+      responseSellerUsecasecc = {
+          error: 'ya existe este numero de cedula'
+        };
+        return responseSellerUsecasecc;
+
+      default: // Caso default cuando no hay coincidencias 
+        responseBD = this.sellerGateway.save(param);
+        return responseBD;
     }
+
+    
 
   }
 }
+
